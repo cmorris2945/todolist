548544781106f4866127a2bc5a74958b4de89055
cmorris2945 <cmorris2945@yahoo.com>
2023-02-08 01:26:27 -0500

    This is a simple program to create a todo list for things you need to get done.

---
+++ main.py
@@ -0,0 +1,19 @@
+
+
+todos = []
+num = 0
+while True:
+    user_action = input("Type add or show Mac: ")
+    user_action = user_action.strip()
+
+    match user_action:
+        case 'add':
+            todo = input("Enter a todo: ")
+            todos.append(todo)
+        case 'show':
+            for item in todos:
+                print(item)
+        case 'exit':
+            break
+
+
