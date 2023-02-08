61f3e8316a23fe3f4a0cf4e37f07b042fd35731e
cmorris2945 <cmorris2945@yahoo.com>
2023-02-08 14:42:49 -0500

    I just modfied the code a little to put in an "edit" feature.

--- main.py
+++ main.py
@@ -3,7 +3,8 @@
 todos = []
 num = 0
 while True:
-    user_action = input("Type add or show Mac: ")
+    user_action = input("Welcome to the task todo list program. Just type what you want to do: add, show. edit,"
+                        " or exit...")
     user_action = user_action.strip()
 
     match user_action:
@@ -13,6 +14,12 @@ while True:
         case 'show':
             for item in todos:
                 print(item)
+        case 'edit':
+            number = int(input("Enter the number in the todo list to edit..."))
+            number = number -1
+            new_todo = input("Enter the new todo...")
+            todos[number] = new_todo
+
         case 'exit':
             break
 
