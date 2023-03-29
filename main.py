c19fa5b0b3926183583dda05d824c68b61306866
cmorris2945 <cmorris2945@yahoo.com>
2023-03-28 23:12:36 -0400

    I just modfied the code a little to put in an "edit" feature.

--- main.py
+++ main.py
@@ -4,69 +4,55 @@ def get_todos():
     return todos
 
 while True:
-        user_action = input("Welcome to the task todo list program. Just type what you want to do: add, show, edit,"
-                            "completed, or exit...")
-        user_action = user_action.strip()
+    user_action = input("Welcome to the task todo list program. Just type what you want to do: add, show, edit, completed, or exit...")
+    user_action = user_action.strip()
 
+    if user_action.startswith("add"):
+        new_todo = input("Enter the new todo...")
+        todos = get_todos()
+        todos.append(new_todo + '\n')
+        with open('file/subfiles/todos.txt', 'w') as file:
+            file.writelines(todos)
 
-        if user_action.startswith("add"):
-            todos = user_action[4:]
+    elif user_action.startswith("show"):
+        todos = get_todos()
+        for index, item in enumerate(todos):
+            item = item.strip('\n')
+            row = f"{index+1}-{item}"
+            print(row)
 
+    elif user_action.startswith("edit"):
+        try:
+            number = int(user_action[5:])
+            number = number - 1
             todos = get_todos()
-
-            todos.append(str(todos) + '\n')
-
+            new_todo = input("Enter the new todo...")
+            todos[number] = new_todo + '\n'
             with open('file/subfiles/todos.txt', 'w') as file:
                 file.writelines(todos)
+        except ValueError:
+            print("Your command is not valid")
+            continue
 
-        elif user_action.startswith("show"):
-
+    elif user_action.startswith("completed"):
+        try:
+            number = int(user_action[9:])
             todos = get_todos()
+            index = number - 1
+            todo_to_remove = todos[index].strip('\n')
+            todos.pop(index)
+            with open('file/subfiles/todos.txt', 'w') as file:
+                file.writelines(todos)
+            message = f"Todo {todo_to_remove} was removed from the list"
+            print(message)
+        except IndexError:
+            print("There is no item with that number chump! Try again...")
+            continue
 
-            todos.append(str(todos) + '\n')
+    elif user_action.startswith("exit"):
+        break
 
-            for index, item in enumerate(todos):
-                item = item.strip('\n')
-                row = f"{index+1}-{item}"
-                print(row)
-        elif user_action.startswith("edit"):
-            try:
-                number =int(user_action[5:])
-                print(number)
-                number = number -1
+    else:
+        print("Command is not valid. Please enter a valid command....")
 
-                todos = get_todos()
-
-                new_todo = input("Enter the new todo...")
-                todos[number] = new_todo + '\n'
-
-                with open('todos.txt', 'w') as file:
-                    file.writelines(todos)
-            except ValueError:
-                print("Your command is not valid")
-                continue
-
-        elif user_action.startswith("completed"):
-            try:
-                number = int(user_action[9:])
-
-                todos = get_todos()
-                index = number -1
-                todo_to_remove = todos[index].strip('\n')
-                todos.pop(index)
-
-                with open('file/subfiles/todos.txt', 'w') as file:
-                    file.writelines(todos)
-
-                message = f"Todo {todo_to_remove} was removed from the list"
-                print(message)
-            except IndexError:
-                print("There is no item with that number chump! Try again...")
-                continue
-
-
-        elif user_action.startswith("exit"):
-            break
-        else:
-            print("Command is not valid. Please enter a valid command....")
-print("get lost Jack!")
+print("Goodbye!")
