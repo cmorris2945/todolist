f3a9ef5e92896118a5d938892c99019542297a6c
cmorris2945 <cmorris2945@yahoo.com>
2023-05-07 16:39:32 -0400

    I implemented a "Add new todo" feature to the application program

--- functions.py
+++ functions.py
@@ -5,10 +5,17 @@ def get_todos(filepath=FILEPATH):
     """Reads a text file and
     returns the list of to-do items"""
     with open(filepath, 'r') as file_local:
-        todos_local = file_local.readlines()
+        todos_local = [line.strip() for line in file_local.readlines()]
     return todos_local
+
 
 def write_todos(todos_arg, filepath=FILEPATH):
     """ Writes the to-do items int eh list in the text files"""
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """ Writes the to-do items in the list to the text files"""
+    with open(filepath, 'w') as file:
+        for todo in todos_arg:
+            file.write(todo + '\n')
--- web.py
+++ web.py
@@ -1,14 +1,25 @@
 import streamlit as st
-import functions
+from functions import *
+def add_todo():
+    todo = st.session_state["new_todo"]
+    todos.append(todo)
+    write_todos(todos)
 
-todos = functions.get_todos()
+    #st.experimental_rerun()
+
+todos = get_todos()
+
 
 st.title("My Todo Application")
 st.subheader("This is my todo application")
 st.write("(Hopefully this application with increase your productivity...)")
 
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    st.checkbox(todo, key=f"todo_{index}")
 
-st.text_input(label="", placeholder="Add a new todo...")
+
+st.text_input(label="", placeholder="Add a new todo...",
+              on_change=add_todo, key='new_todo')
+
+print("Hello")
